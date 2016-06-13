457710ec2bd07ef9a72c5c5a4597ab5c217ff22e
Aleksandr Razumov <ar@cydev.ru>
2016-06-13 18:14:59 +0300

    add tests

--- player.go
+++ player.go
@@ -1,10 +1,10 @@
+// Package player implements backend side stream buffering.
 package player
 
 import (
 	"io"
 	"sync"
 
-	log "github.com/Sirupsen/logrus"
 	"github.com/pkg/errors"
 )
 
@@ -27,6 +27,8 @@ const (
 	ErrBufferTooSmall Error = "buffer is too small"
 	// ErrTooBigWrite indicates attempt to write too large data chunk.
 	ErrTooLargeWrite Error = "write is too large"
+	// ErrEmpty means that Buffer is empty.
+	ErrEmpty Error = "buffer is empty"
 )
 
 // Buffer represents in-memory buffer for stream.
@@ -36,16 +38,23 @@ type Buffer struct {
 	count    int64
 	lastID   int64
 	firstID  int64
+	overflow bool
 	l        sync.Mutex
 	data     []byte
 }
 
+// Config is configuration for Buffer.
 type Config struct {
 	Segment int64
 	Count   int64
 	Start   int64
+	// AllowOverflow allows Buffer.Write to accept buffer which size
+	// is larger than maximum internal buffer size (count * segment).
+	AllowOverflow bool
 }
 
+// New creates new Buffer with specified settings. If value in Config is zero,
+// sensible defaults will be used.
 func New(cfg Config) *Buffer {
 	if cfg.Segment == 0 {
 		cfg.Segment = 1024
@@ -57,57 +66,77 @@ func New(cfg Config) *Buffer {
 		segment:  cfg.Segment,
 		maxCount: cfg.Count,
 		firstID:  cfg.Start,
+		overflow: cfg.AllowOverflow,
 	}
 }
 
+// NewDefault returns new buffer with default config.
 func NewDefault() *Buffer {
 	return New(Config{})
 }
 
+// SetCount sets maximum segment count.
 func (b *Buffer) SetCount(count int64) {
 	b.l.Lock()
 	b.maxCount = count
 	b.l.Unlock()
 }
 
+// SegmentSize returns size of segment.
 func (b Buffer) SegmentSize() int64 {
 	return b.segment
 }
 
-func (b Buffer) Count() int64 {
+// Count returns current maximum segment count.
+func (b *Buffer) Count() int64 {
+	b.l.Lock()
+	defer b.l.Unlock()
 	return b.maxCount
 }
 
-func (b Buffer) Size() int {
+// Size returns current data length.
+func (b *Buffer) Size() int {
 	b.l.Lock()
 	defer b.l.Unlock()
 	return len(b.data)
 }
 
-func (b Buffer) getSegment(id int64) []byte {
+// getSegment returns buffer for segment with id. No checks and locks.
+func (b *Buffer) getSegment(id int64) []byte {
 	start := b.segment * (id - b.firstID)
 	return b.data[start : b.segment+start]
 }
 
-// Write appends internal buffer with new data.r
+// Write appends internal buffer with new data.
 func (b *Buffer) Write(buf []byte) (int, error) {
 	if int64(len(buf)) > b.segment*b.maxCount {
-		log.Warnf("write len(buf) %d > %d", len(buf), b.segment*b.maxCount)
-		return 0, errors.Wrap(ErrTooLargeWrite, "failed to write")
+		// buffer length is bigger than maximum size.
+		if !b.overflow {
+			return 0, errors.Wrap(ErrTooLargeWrite, "failed to write")
+		}
 	}
+
 	b.l.Lock()
-	b.data = append(b.data, buf...)
+	b.data = append(b.data, buf...) // ALLOCATIONS: suboptimal.
 	b.count = int64(len(b.data)) / b.segment
-	b.lastID = b.count + b.firstID
+
+	// updating buffer window (firstID and lastID)
+	b.lastID = b.count + b.firstID - 1
+	// while internal buffer size > maximum size
 	for int64(len(b.data)) > b.segment*b.maxCount {
+		// CPU: suboptimal.
 		b.firstID++
 		b.data = b.data[b.segment:]
 	}
+
 	b.l.Unlock()
 	return len(buf), nil
 }
 
 func (b *Buffer) acquireID(id int64) error {
+	if len(b.data) == 0 {
+		return ErrEmpty
+	}
 	if id < b.firstID || id > b.lastID {
 		return ErrMiss
 	}
@@ -130,13 +159,27 @@ func (b *Buffer) ReadID(w io.Writer, id int64) (int, error) {
 // Get writes segment with requested id into buf
 func (b *Buffer) Get(buf []byte, id int64) error {
 	if int64(len(buf)) < b.segment {
-		return ErrBufferTooSmall
+		return errors.Wrap(ErrBufferTooSmall, "bad buffer")
 	}
 	b.l.Lock()
 	if err := b.acquireID(id); err != nil {
 		return errors.Wrap(err, "bad id")
 	}
-	copy(buf, b.getSegment(id))
+	copy(buf[:b.segment], b.getSegment(id))
 	b.l.Unlock()
 	return nil
+}
+
+// LastID returns last segment id.
+func (b *Buffer) LastID() int64 {
+	b.l.Lock()
+	defer b.l.Unlock()
+	return b.lastID
+}
+
+// FirstID returns first segment id.
+func (b *Buffer) FirstID() int64 {
+	b.l.Lock()
+	defer b.l.Unlock()
+	return b.firstID
 }
--- player_test.go
+++ player_test.go
@@ -1,10 +1,11 @@
 package player
 
 import (
-	"testing"
+	"bytes"
+	"github.com/pkg/errors"
 	"io"
 	"math/rand"
-	"bytes"
+	"testing"
 )
 
 var (
@@ -18,7 +19,65 @@ func Rand() io.Reader {
 func TestBuffer_Write(t *testing.T) {
 	r := Rand()
 	b := NewDefault()
-	if _, err := io.CopyN(b, r, b.SegmentSize() * 4); err != nil {
+	if _, err := io.CopyN(b, r, b.SegmentSize()*4); err != nil {
+		t.Error(err)
+	}
+	buf := make([]byte, b.SegmentSize()*b.Count()+b.SegmentSize())
+	if _, err := b.Write(buf); errors.Cause(err) != ErrTooLargeWrite {
+		t.Error(err, "should be", ErrTooLargeWrite)
+	}
+}
+
+func TestBuffer_SetCount(t *testing.T) {
+	b := NewDefault()
+	c := b.Count() + 5
+	buf := make([]byte, c*b.SegmentSize())
+	if _, err := b.Write(buf); errors.Cause(err) != ErrTooLargeWrite {
+		t.Error(err, "should be", ErrTooLargeWrite)
+	}
+	b.SetCount(c)
+	if _, err := b.Write(buf); err != nil {
+		t.Error(err)
+	}
+}
+
+func TestBuffer_Size(t *testing.T) {
+	b := New(Config{
+		Count:   12,
+		Segment: 512,
+	})
+	buf := make([]byte, 10*512)
+	if _, err := b.Write(buf); err != nil {
+		t.Error(err)
+	}
+	if b.Size() != 512*10 {
+		t.Error("bad size")
+	}
+}
+
+func TestBuffer_Collection(t *testing.T) {
+	b := New(Config{
+		Count:   12,
+		Segment: 512,
+	})
+	buf := make([]byte, 12*512)
+	if _, err := b.Write(buf); err != nil {
+		t.Error(err)
+	}
+	buf = buf[:2*512]
+	if _, err := b.Write(buf); err != nil {
+		t.Error(err)
+	}
+}
+
+func TestBuffer_ReadID(t *testing.T) {
+	r := Rand()
+	b := NewDefault()
+	if _, err := io.CopyN(b, r, b.SegmentSize()*4); err != nil {
+		t.Error(err)
+	}
+	buf := new(bytes.Buffer)
+	if _, err := b.ReadID(buf, 3); err != nil {
 		t.Error(err)
 	}
 }
@@ -26,11 +85,55 @@ func TestBuffer_Write(t *testing.T) {
 func TestBuffer_Get(t *testing.T) {
 	r := Rand()
 	b := NewDefault()
-	if _, err := io.CopyN(b, r, b.SegmentSize() * 4); err != nil {
+	if _, err := io.CopyN(b, r, b.SegmentSize()*4); err != nil {
 		t.Error(err)
 	}
-	buf := new(bytes.Buffer)
-	if _, err := b.ReadID(buf, 3); err != nil {
+	buf := make([]byte, b.SegmentSize())
+	if err := b.Get(buf, 3); err != nil {
 		t.Error(err)
+	}
+
+	buf = buf[:b.SegmentSize()/2]
+	if err := b.Get(buf, 3); errors.Cause(err) != ErrBufferTooSmall {
+		t.Error(err, "should be", ErrBufferTooSmall)
+	}
+	buf = buf[:b.SegmentSize()]
+	if err := b.Get(buf, b.Count()*2); errors.Cause(err) != ErrMiss {
+		t.Error(err, "should be", ErrMiss)
+	}
+}
+
+func TestBuffer_ID(t *testing.T) {
+	b := NewDefault()
+	buf := make([]byte, b.SegmentSize()*3)
+	// 0, 1, 2
+	if _, err := b.Write(buf); err != nil {
+		t.Error(err)
+	}
+	if b.LastID() != 2 {
+		t.Error("bad last id", b.LastID())
+	}
+	if err := b.Get(buf, b.LastID()); err != nil {
+		t.Error(err)
+	}
+	if b.FirstID() != 0 {
+		t.Error("bad first id", b.FirstID())
+	}
+}
+
+func TestBuffer_ErrMiss(t *testing.T) {
+	b := NewDefault()
+	buf := new(bytes.Buffer)
+	if _, err := b.ReadID(buf, 1); errors.Cause(err) != ErrEmpty {
+		t.Error(err, "should be", ErrEmpty)
+	}
+}
+
+func TestError(t *testing.T) {
+	if Error("error").Error() != "error" {
+		t.Error("bad Error")
+	}
+	if Error("error").String() != "err: error" {
+		t.Error("bad String for Error")
 	}
 }
